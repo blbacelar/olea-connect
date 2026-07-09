a8a2a33b659ff160c5c13481e7c8f4c3719223f6
Bruno Bacelar <blbacelar@gmail.com>
2026-07-09 13:59:26 -0700

    Include Chromium assets for PDF exports

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    outputFileTracingIncludes: {
+      "/modules/board-calendar": ["./node_modules/@sparticuz/chromium/bin/**/*"],
+      "/templates/[slug]": ["./node_modules/@sparticuz/chromium/bin/**/*"],
+      "/templates/board-self-evaluation": ["./node_modules/@sparticuz/chromium/bin/**/*"],
+    },
     serverComponentsExternalPackages: ["@sparticuz/chromium"],
   },
   reactStrictMode: true,
