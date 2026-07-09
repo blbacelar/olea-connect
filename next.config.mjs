2cbc7d6b7a7940b2b05e2ba3b3aa914dca38eef9
Bruno Bacelar <blbacelar@gmail.com>
2026-07-09 13:43:45 -0700

    Fix board calendar PDF export on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@sparticuz/chromium"],
+  },
   reactStrictMode: true,
 };
 
